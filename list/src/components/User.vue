cf2196af093e6e28b730d92fe764a32c31f1353c
Jesús Ángel Pérez-Roca Fernández <djalma8@gmail.com>
2019-04-25 18:28:13 +0200

    User list completed

---
+++ list/src/components/User.vue
@@ -0,0 +1,10 @@
+<template>
+  <span>{{user.id}} - {{user.name}}</span>
+</template>
+<script>
+export default {
+  props: {
+    user: {}
+  }
+};
+</script>
---
+++ list/src/components/UserList.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <ul>
+      <li v-for="user in users" :key="user.id">
+        <User :user="user"></User>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import User from "./User";
+export default {
+  name: "UserList",
+  props: {
+    msg: String
+  },
+  components: {
+    User
+  },
+  data() {
+    return {
+      users: []
+    };
+  },
+  created() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(json => (this.users = json));
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+li {
+  list-style: none;
+}
+</style>
